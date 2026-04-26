1079d3306c7145c75bc3d4f3201d8b551bbd9645
anvitha1633 <anvithashet@gmail.com>
2026-04-26 17:11:18 +0530

    improve app with env and time

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import os
+from datetime import datetime
+
 print("Hello world from my devops app!")
+def main():
+    print("devops app running")
+    print("Checking for .log files ...\n")
+    count =0
+    for filw in os.listdir('.'):
+        if filw.endswith('.log'):
+            count = count +1
+            print(f"Found log file: {filw}")
+    print(f"Total .log files found: {count}")
+    print(f"Current date and time: {datetime.now()}")
+    print("Environment:",os.getenv("ENVIRONMENT", "Not set"))
+
+if __name__ == "__main__":
+    main()
